91dd4e53be05f61e88b417b73d9124e3441d4cdf
nishu65 <nishubhaiqwerty@gmail.com>
2023-01-31 18:01:57 +0530

    user defined .h file

---
+++ test0.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "addingtwo.h"
+
+int main(){
+    int a,b;
+     printf("ENTER FIRST NO. :  ");
+    scanf("%d", &a); 
+    printf("ENTER SECOND NO. :  ");
+    scanf("%d", &b);
+    sumtwo22(a,b);
+    return 0;
+}
